3138974ac0db5764d1f3163b89ac1a1364836729
cawapy <31598347+cawapy@users.noreply.github.com>
2019-06-20 22:10:25 +0200

    minor renaming, cleanup

--- HamsterGo.ino
+++ HamsterGo.ino
@@ -1,71 +1,80 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-const int irPin = 3;
-const int ledPin = 13;
+const uint8_t wheelCircumferenceCm = 87;
+const uint32_t speedometerTimeoutMs = 5000ul;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+const uint8_t sensorPin = 3;
+
+const uint8_t lcdColumns = 20;
+const uint8_t lcdRows = 4;
+const uint8_t lcdI2cAddress = 0x27;
+
+LiquidCrystal_I2C lcd(lcdI2cAddress, lcdColumns, lcdRows);
 
 void setup()
 {
-  pinMode(irPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  lcd.begin();
-
-  lcd.backlight();
-  lcd.clear();
-  lcd.print("Hamster, go!");
+    pinMode(sensorPin, INPUT);
+    lcd.begin();
+    lcd.backlight();
+    lcd.clear();
+    lcd.print("Hamster, go!");
 }
 
-uint32_t count = 0;
-uint32_t lastMs = 0;
+uint32_t revolutionCount = 0;
+uint32_t lastRevolutionTimestampMs = 0;
 void loop()
 {
-  const uint8_t circumFerenceCm = 87;
-  bool update = false;
-  uint32_t tNow = millis();
-  uint32_t deltaMs = 0;
-  if (detectStep()) {
-    count++;
-    deltaMs = tNow - lastMs;
-    lastMs = tNow;
-    update = true;
+  bool needUpdate = false;
+  uint32_t timestampMs = millis();
+
+  uint32_t revolutionDurationMs;
+
+  if (detectStep())
+  {
+    revolutionCount++;
+    revolutionDurationMs = timestampMs - lastRevolutionTimestampMs;
+    lastRevolutionTimestampMs = timestampMs;
+    needUpdate = true;
   }
-  if ((tNow - lastMs) > 5000ul) {
-    deltaMs = 0;
-    update = true;
+
+  if ((timestampMs - lastRevolutionTimestampMs) > speedometerTimeoutMs)
+  {
+    revolutionDurationMs = 0;
+    needUpdate = true;
   }
-  if (update)
+
+  if (needUpdate)
   {
     char buff[21];
-    sprintf(buff, "Runden: %12d", count);
+    sprintf(buff, "Runden: %12d", revolutionCount);
     lcd.setCursor(0, 0);
     lcd.print(buff);
  
     lcd.setCursor(0, 3);
-    if (deltaMs) {
+    if (revolutionDurationMs != 0) {
       const uint32_t msPerHour = 1000ul * 60ul * 60ul;
       const uint32_t cmPerHundredMeters = 10000ul;
-      uint16_t hundredMetersPerHour = circumFerenceCm * msPerHour / deltaMs / cmPerHundredMeters;
+      uint16_t hundredMetersPerHour = wheelCircumferenceCm * msPerHour / revolutionDurationMs / cmPerHundredMeters;
       sprintf(buff, "Tempo:     %2d.%01d km/h", hundredMetersPerHour / 10, hundredMetersPerHour % 10);
- 
       lcd.print(buff);
     }
     else {
       lcd.print("Tempo:      0.0 km/h");
     }
-    uint16_t distanceCm = circumFerenceCm * count;
+    uint16_t distanceCm = wheelCircumferenceCm * revolutionCount;
     sprintf(buff, "Strecke: %6d.%02d m", distanceCm / 100, distanceCm % 100);
     lcd.setCursor(0, 1);
     lcd.print(buff);
   }
 }
 
+
 bool current = true;
 bool detectStep()
 {  
   bool previous = current;
-  current = digitalRead(irPin) == HIGH;
+  current = digitalRead(sensorPin) == HIGH;
   if (current != previous) delay(5);
   return current && !previous;
 }
